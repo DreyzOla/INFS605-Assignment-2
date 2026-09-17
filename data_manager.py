cd6af2c57a42a8d0a925b405af9e1457f0a3bb18
olusogaolabisi <dreyz.bsu@gmail.com>
2026-09-16 22:12:22 -0400

    Code to handle Load, append  and Save application data

--- data_manager.py
+++ data_manager.py
@@ -1,2 +1,14 @@
-CSV read, write, update, delete functions
+# CSV read, write, update, delete functions
+import csv
+
+FILE_NAME = "applications.csv"
+
+def load_applications():
+    with open(FILE_NAME, "r") as file:
+        return list(csv.reader(file))
+
+def save_application(app_name, category, department, url):
+    with open(FILE_NAME, "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([app_name, category, department, url])
 
